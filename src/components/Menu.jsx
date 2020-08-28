8e24788a0e9fa61ca7856b399cb1436309e52013
Abdallah Sayed <34820902+abdallah-95@users.noreply.github.com>
2020-08-28 16:36:58 +0200

    commit 2882020_1

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -48,23 +48,24 @@ class Menu extends React.Component{
                 <div>
                     <div id="mySidenav" className="sidenav">
                         <a href="javascript:void(0)" className="closebtn" onClick={this.closeNav}>&times;</a>                      
-                        <Link to={process.env.PUBLIC_URL + "/"}>Home</Link>                        
-                        <Link to={process.env.PUBLIC_URL + "/Calculator"}>Calculator</Link>
-                        <Link to={process.env.PUBLIC_URL + "/Counters"}>Counters</Link>
-                        <Link to={process.env.PUBLIC_URL + "/TicTacToe"}>Tic-Tac-Toe</Link>
-                        <Link to={process.env.PUBLIC_URL + "/GoogleDictionary"}>Google Dictionary</Link>
+                        <Link to="/">Home</Link>                        
+                        <Link to="/Calculator">Calculator</Link>
+                        <Link to="/Counters">Counters</Link>
+                        <Link to="/TicTacToe">Tic-Tac-Toe</Link>
+                        <Link to="/GoogleDictionary">Google Dictionary</Link>
+                        <Link to="/E-Learning">E-Learning</Link>
                         </div>
 
                         <div id="main">
                         <h2>React Examples</h2>
                         <span style={{fontSize:"30px",cursor:"pointer"}} onClick={this.toggleMenu}>&#9776; open</span>
                         <div id="content">
-                            <Route path={process.env.PUBLIC_URL + "/"}  component={this.Index} />
-                            <Route path={process.env.PUBLIC_URL + "/Calculator"} component={Calculator} />
-                            <Route path={process.env.PUBLIC_URL + "/Counters"} component={Counters} />
-                            <Route path={process.env.PUBLIC_URL + "/TicTacToe"} component={TicTacToeGame} />
+                            <Route path="/"  component={this.Index} />
+                            <Route path="/Calculator" component={Calculator} />
+                            <Route path="/Counters" component={Counters} />
+                            <Route path="/TicTacToe" component={TicTacToeGame} />
                             {/* <Route path="/GoogleDictionary" component={Dictionary} /> */}
-                            <Route path={process.env.PUBLIC_URL + "/GoogleDictionary"} component={() => <Dictionary MainMenuState={this.state.menuOpened}/>} />
+                            <Route path="/GoogleDictionary" component={() => <Dictionary MainMenuState={this.state.menuOpened}/>} />
                             {/* <Route path="/E-Learning" component={() => <ELearningMain handleELMenuStateChange={this.handleELMenuStateChange}/>} /> */}
                             {/* <Route path="/E-Learning" component={ELearningMain} /> */}
                         </div>
